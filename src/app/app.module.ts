a100fbc0e74dbe711b55eeef5561c30a7caf0e46
Jeffpultz <Jeff@denn.dk>
2022-11-15 15:25:00 +0100

    All the basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FrontpageComponent } from './pages/frontpage/frontpage.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FrontpageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
