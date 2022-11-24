2b2636572d7bd853d08e443956e31c9074db9ccb
Jeffpultz <Jeff@denn.dk>
2022-11-24 16:49:18 +0100

    Login AS pretty as Daniel!!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FrontpageComponent } from './pages/frontpage/frontpage.component';
 import { ListComponent } from './pages/list/list.component';
+import { LoginComponent } from './pages/login/login.component';
 import { Top10Component } from './pages/top10/top10.component';
 
 const routes: Routes = [
   { path: '', component: FrontpageComponent},
   { path: 'top10', component: Top10Component},
-  {path: 'list', component: ListComponent},
+  { path: 'list', component: ListComponent},
+  { path: 'login', component: LoginComponent},
 ];
 
 @NgModule({
