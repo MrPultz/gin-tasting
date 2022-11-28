c96bfe3a201dd2a546b4dc70aa7a644a8a590749
Daniel Møller Svensson <202007248@post.au.dk>
2022-11-28 20:26:36 +0100

    Added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FrontpageComponent } from './pages/frontpage/frontpage.component';
@@ -10,6 +11,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { GinRenderComponent } from './components/gin-render/gin-render.component';
 import { ListComponent } from './pages/list/list.component';
 import { Top10Component } from './pages/top10/top10.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -24,9 +32,19 @@ import { Top10Component } from './pages/top10/top10.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    providePerformance(() => getPerformance()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'gin-tasting',
+    appId: '1:591522300392:web:e56b1c783f72e2581fe45d',
+    storageBucket: 'gin-tasting.appspot.com',
+    apiKey: 'AIzaSyD9UVeOvxu74O35pTKVo5Fok3d-2X92uFo',
+    authDomain: 'gin-tasting.firebaseapp.com',
+    messagingSenderId: '591522300392',
+    measurementId: 'G-W9FK3K6E83',
+  },
   production: true
 };
