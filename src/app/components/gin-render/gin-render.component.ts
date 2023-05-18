6757cdad910d585173193d40de50c76fa3a695db
Daniel Møller Svensson <202007248@post.au.dk>
2023-05-18 21:40:21 +0200

    Wrong path for gin model in gin render

--- src/app/components/gin-render/gin-render.component.ts
+++ src/app/components/gin-render/gin-render.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Gin } from 'src/app/models/gin';
+import { Gin } from 'src/app/models/Gin';
 
 @Component({
   selector: 'gin-render',
