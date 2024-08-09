24b3f58e1013776f77b51cb418ea9487b16b1f96
Daniel <dmsdevelop100@gmail.com>
2024-08-09 22:07:39 +0200

    Updated environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,16 +2,10 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { firebase } from "./firebaseConfig";
+
 export const environment = {
-  firebase: {
-    projectId: 'gin-tasting',
-    appId: '1:591522300392:web:e56b1c783f72e2581fe45d',
-    storageBucket: 'gin-tasting.appspot.com',
-    apiKey: 'AIzaSyD9UVeOvxu74O35pTKVo5Fok3d-2X92uFo',
-    authDomain: 'gin-tasting.firebaseapp.com',
-    messagingSenderId: '591522300392',
-    measurementId: 'G-W9FK3K6E83',
-  },
+  firebase: firebase,
   production: false
 };
 
